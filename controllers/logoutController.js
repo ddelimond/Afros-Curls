45b0448949f76a6bbc03b20c98a5d01371726afa
Darren Delimond <darrendelimond@gmail.com>
2022-12-15 11:31:22 -0500

    =added logout, and fixed registration and login

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
 module.exports = (req, res, next) => {
     req.logout((logUserOut) => {
         if (logUserOut) { return next(logUserOut) }
--- routes/lookbook.js
+++ routes/lookbook.js
@@ -1,3 +1,5 @@
 const express = require('express')
 const router = express.Router()
 const lookbookController = require('../routes/')
+
+router.use('/', lookbookController.goToLookbooks)
