637a9f60a09af7169b7ca829eb7cf03033dc2cda
Darren Delimond <darrendelimond@gmail.com>
2022-12-18 12:20:50 -0500

    =fixed lookbook route

--- routes/lookbook.js
+++ routes/lookbook.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const lookbookController = require('../controllers/lookbookController')
 
-router.get('/', lookbookController.goToLookbooks)
+router.get('/', lookbookController.goToLookbook)
 
 module.exports = router
