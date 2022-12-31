298d687b0471ea5bf768e44ad2a89412b325ead1
Darren Delimond <darrendelimond@gmail.com>
2022-12-30 22:49:54 -0500

    =removed microsoft login with facebook

--- routes/auth.js
+++ routes/auth.js
@@ -43,11 +43,12 @@ router.get('/facebook',
     passport.authenticate('facebook'));
 
 router.get('/facebook/callback',
-    passport.authenticate('facebook', { failureRedirect: '/login' }),
-    function (req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/dashboard');
-    });
+    passport.authenticate('facebook', {
+        successRedirect: '/dashboard',
+        failureRdirect: '/login',
+        session: false
+    }))
+
 
 
 
